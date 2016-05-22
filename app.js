8227a4cc137ef99e7d6cbc4388620f875bccbd7f
james <jamesraveloc52@gmail.com>
2016-05-21 21:51:28 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var express = require("express");
+
+//put the folders in the server 
+app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/scripts/bootstrap.min.js'));
+app.use(express.static(__dirname + '/scripts/jquery-1.12.3.min.js'));
+app.use(express.static(__dirname + '/scripts/main.js'));
+//app.use(express.static(__dirname + '/img'));
+//app.use(express.static(__dirname + '/css'));
+//app.use(express.static(__dirname + '/fonts'));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+//connection, disconnection and bi-direccional events of communication
+io.on('connection', function(socket){
+  
+  console.log('user connected');
+
+
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+
+
+});
+
+//Start the server on the port specified
+//http.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
+//  console.log('Magic running on 8080');
+//});
+
+
+http.listen(8080, function(){
+  console.log('Magic running on 8080');
+});
+
