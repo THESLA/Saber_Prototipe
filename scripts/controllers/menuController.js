7e9024ac6fcf5055113add6710a041e45e60532e
james <jamesraveloc52@gmail.com>
2016-06-08 10:00:29 -0500

    se agrago el menu a toda web

---
+++ scripts/controllers/menuController.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    var controller = app.controller('menuController', menuController);
+    //angular.module('productManagement')
+    //    .controller('welcome', welcome);
+
+    menuController.$inject = [ '$scope'];
+    function menuController($scope) {
+
+
+    }
+
+})();
