88a88eaca53be9a99bdacbb459ea90e22d87b5f3
james <jamesraveloc52@gmail.com>
2016-06-10 12:45:45 -0500

    se agrego dataTablet con bace es javascript

--- scripts/controllers/resultadosController.js
+++ scripts/controllers/resultadosController.js
@@ -8,7 +8,32 @@
     resultadosController.$inject = [ '$scope'];
     function resultadosController($scope) {
     
-    $('#example').DataTable();
+    var dataSet = [
+    [ "Tiger Nixon", "6To", "000000000", "12", "313243634x" ],
+    [ "Pedro Shause", "6To", "000000001", "14", "313243634x" ],
+    [ "Ramon Martinez", "6To", "000000002", "15", "313243634x" ],
+    [ "Magda Sarate", "6To", "000000003", "17", "313243634x" ],
+    [ "Dario Ramos", "6To", "000000004", "13", "313243634x" ],
+    [ "Victor Blash", "6To", "000000005", "15", "313243634x" ],
+    [ "BlashT Rita", "5To", "000000006", "13", "313243634x" ],
+    [ "Tirion Lanister", "3Ro", "000000007", "11", "313243634x" ],
+    [ "Samsa Satark", "2Do", "000000008", "15", "313243634x" ],
+];
+
+
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Grado" },
+            { title: "ID" },
+            { title: "Edad" },
+            { title: "telefono" },
+        ]
+    } );
+} );
 
     }
 
