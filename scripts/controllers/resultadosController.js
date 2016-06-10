014699b20b423efff7ea25ffc5f863e4aa161afa
james <jamesraveloc52@gmail.com>
2016-06-10 17:51:38 -0500

    Arregos menores

--- scripts/controllers/resultadosController.js
+++ scripts/controllers/resultadosController.js
@@ -9,15 +9,15 @@
     function resultadosController($scope) {
     
     var dataSet = [
-    [ "Tiger Nixon", "6To", "000000000", "12", "313243634x" ],
-    [ "Pedro Shause", "6To", "000000001", "14", "313243634x" ],
-    [ "Ramon Martinez", "6To", "000000002", "15", "313243634x" ],
-    [ "Magda Sarate", "6To", "000000003", "17", "313243634x" ],
-    [ "Dario Ramos", "6To", "000000004", "13", "313243634x" ],
-    [ "Victor Blash", "6To", "000000005", "15", "313243634x" ],
-    [ "BlashT Rita", "5To", "000000006", "13", "313243634x" ],
-    [ "Tirion Lanister", "3Ro", "000000007", "11", "313243634x" ],
-    [ "Samsa Satark", "2Do", "000000008", "15", "313243634x" ],
+    [ "1","Tiger Nixon", "6To", "000000000", "12", "313243634x" ],
+    [ "2","Pedro Shause", "6To", "000000001", "14", "313243634x" ],
+    [ "3","Ramon Martinez", "6To", "000000002", "15", "313243634x" ],
+    [ "4","Magda Sarate", "6To", "000000003", "17", "313243634x" ],
+    [ "5","Dario Ramos", "6To", "000000004", "13", "313243634x" ],
+    [ "6","Victor Blash", "6To", "000000005", "15", "313243634x" ],
+    [ "7","BlashT Rita", "5To", "000000006", "13", "313243634x" ],
+    [ "8","Tirion Lanister", "3Ro", "000000007", "11", "313243634x" ],
+    [ "9","Samsa Satark", "2Do", "000000008", "15", "313243634x" ],
 ];
 
 
@@ -26,6 +26,7 @@ $(document).ready(function() {
     $('#example').DataTable( {
         data: dataSet,
         columns: [
+        	{ title: "#" },
             { title: "Name" },
             { title: "Grado" },
             { title: "ID" },
